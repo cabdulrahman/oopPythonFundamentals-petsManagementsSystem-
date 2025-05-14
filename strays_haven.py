9983c13671b16af9257d50707bcdfe8731434a8b
abdulrahman <abdulrahmanibnimohammed@gmail.com>
2025-05-14 10:31:41 +0300

    added init

---
+++ strays_haven.py
@@ -0,0 +1,30 @@
+#class initialization with deafult__init__
+class Pet:
+      
+    def speak(self):
+      print("sound made")
+Rasmus = Pet()
+Rasmus.name = "Rasmus"
+print(Rasmus.name)
+print(Rasmus.speak())
+class Dog:
+    def __init__(self, name,breed,age = "N/A"):
+        self.name = name
+        self.breed = breed
+        self.age = age
+    def speak(self):
+       return f"{self.name} says woof! woof!"
+koba = Dog("Koba", "Great Done", 3)
+amad= Dog("Amad", "Black Goat")
+koba.age = 4
+print(koba.speak())
+print(koba.age)
+print(amad.age)
+
+class Cat:
+    pass
+    
+
+class Rat:
+    pass
+
