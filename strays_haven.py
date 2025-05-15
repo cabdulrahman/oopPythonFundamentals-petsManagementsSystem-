1f333366f327a35bf5706cfc71b3fb7f0ec46622
abdulrahman <abdulrahmanibnimohammed@gmail.com>
2025-05-15 10:39:55 +0300

    encupsulation showcased

--- strays_haven.py
+++ strays_haven.py
@@ -1,9 +1,35 @@
 #class initialization with deafult__init__
 class Pet:
-      
+#this is the parent and base class for all pets
+    def __init__(self, name, age=None):
+        #protected attributes(encapsulation)
+        self._name = name
+        self.age = age
+        #getter and setter methods for encapsulation
+    def get_name(self):
+        print("Retrieving age")
+        return self._name
+    def set_age(self, age):
+        if type (age) in (int, float) and (age>= 0 and age<=120):
+            self.age = age
+        else:
+            print("Age must be greaterthan 0 or less than 120")
+            #encapsulated age property
+            age = property(get_name, set_age)
+    
     def speak(self):
-      print("sound made")
-Rasmus = Pet()
+     # print("sound made")
+     return("pet spoke")
+
+
+class Cat:
+    pass
+    
+
+class Rat:
+    pass
+
+Rasmus = Pet("Scobby", 2)
 Rasmus.name = "Rasmus"
 print(Rasmus.name)
 print(Rasmus.speak())
@@ -24,10 +50,7 @@ print(koba.speak())
 print(koba.age)
 print(amad.age)
 
-class Cat:
-    pass
-    
-
-class Rat:
-    pass
-
+#demonstrating encapsulation
+print(f"pet's age :{Rasmus.age}")
+Rasmus.age = 5
+print(f"pet's age :{Rasmus.age}")
